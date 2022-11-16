e310608e7fbb365f78ed733fff5015526be2d944
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-17 01:42:33 +0530

    Reverse Number

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ReverseNumber
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                int number = 359;
+
+                int reverseNumber = 0;
+                int remainder = 0;
+                Console.WriteLine("Number before reverse = " + number);
+
+                for (remainder = 0; number > 0; number = number / 10)
+                {
+                    remainder = number % 10;
+
+                    reverseNumber = reverseNumber * 10 + remainder;
+                }
+                Console.WriteLine("Number after reverse  = " + reverseNumber);
+            }
+        }
+    }
